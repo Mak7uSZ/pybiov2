fd476cb8ab11c4cf7e25981cbd581949acdcde52
mak7usz <toys.tb116@gmail.com>
2024-12-19 13:52:38 +0100

    ui main menu

--- part1.py
+++ part1.py
@@ -1,4 +1,5 @@
 from ursina import *
+from multiprocessing import Array, Value
 import socket
 
 # Класс для проверки подключения
@@ -11,48 +12,41 @@ class Connection:
         except:
             return False
 
-# Класс игры
-class Game(Entity):
-    def __init__(self):
-        super().__init__()
-        self.enabled = True
-        self.player = Entity(model='cube', color=color.red, position=(0, 0, 0))
-        self.camera.position = (0, 5, -10)
-        self.camera.look_at(self.player)
-
-    def update(self):
-        self.player.rotation_y += 1  # Поворот игрока для примера
-
 # Класс главного меню
 class MainMenu(Entity):
-    def __init__(self):
+    def __init__(self, ip_var, port_var, status_var):
         super().__init__()
-        self.enabled = True
+        self.ip_var = ip_var
+        self.port_var = port_var
+        self.status_var = status_var
+
+        # Поля ввода IP и порта
         self.ip_input = InputField(text='127.0.0.1', position=(-0.4, 0), scale=(0.5, 0.05))
         self.port_input = InputField(text='8080', position=(0.2, 0), scale=(0.5, 0.05))
+        
+        # Кнопка подключения
         self.connect_button = Button(text="Подключиться", position=(0, -0.1), scale=(0.3, 0.1), on_click=self.connect)
+        
+        # Текст статуса подключения
         self.connection_status = Text(text='', position=(0, -0.2), origin=(0, 0), color=color.red)
-    
+
     def connect(self):
         ip = self.ip_input.text
         port = self.port_input.text
 
+        # Проверяем подключение
         if Connection.test_connection(ip, port):
             self.connection_status.text = "Подключение успешно!"
-            self.enabled = False  # Скрыть главное меню
-            self.start_game()  # Запуск игры после успешного подключения
+
+            # Сохраняем данные в общие переменные
+            self.ip_var.value = ip.encode('utf-8')
+            self.port_var.value = port.encode('utf-8')
+            self.status_var.value = 1  # Устанавливаем статус успешного подключения
         else:
             self.connection_status.text = "Ошибка подключения. Проверьте IP и порт."
-    
-    def start_game(self):
-        # Переключаемся на игровую сцену
-        game_scene = Game()  # Создаем экземпляр игры
-        self.enabled = False  # Скрываем главное меню
-        game_scene.enabled = True  # Показываем сцену игры
 
-app = Ursina()
-
-# Создание главного меню
-main_menu = MainMenu()
-
-app.run()
+# Функция для запуска первой части кода
+def run_part1(ip_var, port_var, status_var):
+    app = Ursina()
+    main_menu = MainMenu(ip_var, port_var, status_var)
+    app.run()
